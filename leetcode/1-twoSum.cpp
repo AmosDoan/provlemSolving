19e70e16d8ca18a927b40e27a952b677c44ae494
amos_doan <amos.doan@gmail.com>
2020-01-02 01:26:08 +0900

    - Add solution 2 for 1-twoSum

--- leetcode/1-twoSum.cpp
+++ leetcode/1-twoSum.cpp
@@ -3,9 +3,11 @@
 //
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
+// O(n^2)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -20,9 +22,28 @@ public:
     }
 };
 
+class Solution2 {
+private:
+    map<int, int> hash;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            auto it = hash.find(need);
+            if (it != hash.end()) {
+                return vector<int>{it->second, i};
+            }
+
+            hash[nums[i]] = i;
+        }
+
+        return vector<int>{};
+    }
+};
 
 int main() {
-    vector<int> numbers = vector<int> {2, 7, 11, 5};
+    vector<int> numbers = vector<int>{2, 7, 11, 5};
 
     for (auto it : numbers) {
         cout << it << " ";
@@ -33,6 +54,13 @@ int main() {
     vector<int> solution = sol.twoSum(numbers, 9);
 
     for (auto it : solution) {
+        cout << it << " ";
+    }
+
+    Solution2 sol2;
+    vector<int> solution2 = sol2.twoSum(numbers, 9);
+
+    for (auto it : solution2) {
         cout << it << " ";
     }
 }
