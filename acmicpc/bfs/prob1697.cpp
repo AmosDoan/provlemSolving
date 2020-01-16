de550bd53f65c5f3d04191ac9cd24080e49aea87
amos_doan <amos.doan@gmail.com>
2020-01-16 22:17:44 +0900

    - Solving prob1697 -2

--- acmicpc/bfs/prob1697.cpp
+++ acmicpc/bfs/prob1697.cpp
@@ -4,14 +4,14 @@
 
 #include <iostream>
 #include <queue>
-
-#define MAX_NUM 100000
+#include <set>
 
 using namespace std;
 
 int start;
 int target;
 queue<pair<int, int>> q;
+set<int> visit;
 
 int solve() {
     q.push({start, 0});
@@ -21,22 +21,35 @@ int solve() {
         int time = q.front().second;
         q.pop();
 
+        visit.insert(current);
+
         if (current == target) {
             return time;
         }
 
-        if (target <= 1.5 * current && target < MAX_NUM) {
-            q.push({current - 1, time + 1});
-            continue;
+        int candidate[4];
+        candidate[0] = current + 1;
+        candidate[1] = current - 1;
+        candidate[2] = 2 * current;
+        candidate[3] = 2 * (current - 1);
+
+        int min = 200000;
+        int min_idx = 0;
+        for (int i = 0; i < 4; i++) {
+            int difference = target - candidate[i];
+            if (difference < min) {
+                min = difference;
+                min_idx = i;
+            }
         }
 
-        if (current > 0) {
-            q.push({current - 1, time + 1});
+        int next_time = time + 1;
+        if (min_idx == 3) {
+            next_time++;
         }
 
-        int teleport = current * 2;
-        if (teleport < MAX_NUM && 1.5 * current < target) {
-            q.push({current * 2, time + 1});
+        if (visit.find(candidate[min_idx]) == visit.end()) {
+            q.push({candidate[min_idx], next_time});
         }
     }
 
