13a90b12ed63bdf945c90ebfcf3fe092409f95c4
amos <amos.doan@gmail.com>
2020-02-21 08:29:33 +0900

    - Solved leetcode 7-reverseInteger

--- leetcode/7-reverseInteger.cpp
+++ leetcode/7-reverseInteger.cpp
@@ -2,16 +2,31 @@
 // Created by Amos on 2020/02/19.
 //
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
+        bool isPositive = true;
+        if (x < 0) {
+            isPositive = false;
+        }
+
         int result = 0;
         while (x != 0) {
             int remainder = x % 10;
             x = x / 10;
+
+            if (isPositive && 0.1 * (INT_MAX - remainder) < result) {
+                return 0;
+            }
+
+            if (!isPositive && 0.1 * (INT_MIN - remainder) > result) {
+                return 0;
+            }
+
             result = result * 10 + remainder;
         }
 
@@ -22,13 +37,13 @@ public:
 int main() {
     Solution *s;
     s = new Solution();
+    cout << s->reverse(-123) << '\n';
+    delete s;
+
+    s = new Solution();
     cout << s->reverse(123) << '\n';
     delete s;
 
-    /*
-    s = new Solution();
-    cout << s->reverse(-123) << '\n';
-    delete s;
 
     s = new Solution();
     cout << s->reverse(1) << '\n';
@@ -38,11 +53,8 @@ int main() {
     cout << s->reverse(1534236469) << '\n';
     delete s;
 
-    cout << sizeof(int) << '\n';
-    cout << sizeof(int *) << '\n';
     cout << INT_MAX << '\n';
     cout << INT_MIN << '\n';
-    */
 
     return 0;
 }
