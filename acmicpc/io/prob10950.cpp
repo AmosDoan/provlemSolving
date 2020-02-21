e8a13b0788dc55e46ced0f73545d1a475cb61d57
amos <amos.doan@gmail.com>
2020-02-21 12:09:33 +0900

    - Solved prob10950

---
+++ acmicpc/io/prob10950.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Amos on 2020/02/21.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
+
