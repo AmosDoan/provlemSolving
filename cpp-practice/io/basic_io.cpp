298d849d32a3734e3c70fd4558adc8e9ccfd3429
amos_doan <amos.doan@gmail.com>
2020-02-22 11:15:39 +0900

    - Update basic_io

--- cpp-practice/io/basic_io.cpp
+++ cpp-practice/io/basic_io.cpp
@@ -7,10 +7,12 @@
 using namespace std;
 
 /**
+ * // 마지막에 엔터가 있어야 된다.
 3
 test1
 test2
 test3
+
  */
 
 int N;
@@ -21,9 +23,13 @@ int main() {
         string test;
         cin >> test;
 
-        // cout이 한번 들어가는 순간 clion에서 마지막에 엔터를 입력해야함
+        // input의 마지막에 개행이 없는 경우 cout이 한번 들어가는 순간 clion에서 마지막에 엔터를 입력해야함
+        // 즉 엔터는 input에 대한 개행
+        cout << test << '\n';
+        /*
         if (test == "test1") {
             cout << test << '\n';
         }
+         */
     }
 }
