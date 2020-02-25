221d67d38ecf7bc853327460bb8e37417aee39cc
amos_doan <amos.doan@gmail.com>
2020-02-26 01:30:39 +0900

    - Solved leetcode intergerToRoman

--- cpp-practice/string/string_practice.cpp
+++ cpp-practice/string/string_practice.cpp
@@ -3,15 +3,38 @@
 //
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+// https://stackoverflow.com/questions/35506712/string-multiplication-in-c
+/*
 int main() {
     string s;
 
     // 반복이 아님..
     s.append("t", 2);
     s.append("t", 20);
+
+    cout << s << '\n';
+}
+*/
+
+string operator*(const string& str, int times) {
+    stringstream stream;
+
+    for (size_t i = 0; i < times; i++) {
+        stream << str;
+    }
+    return stream.str();
+}
+
+int main() {
+    string s;
+    string test1 = "tt";
+    string repeated = test1 * 4;
+
+    s.append(repeated);
 
     cout << s << '\n';
 }
--- leetcode/12-integerToRoman.cpp
+++ leetcode/12-integerToRoman.cpp
@@ -2,61 +2,64 @@
 // Created by Amos on 2020/02/24.
 //
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+// Retrospect : https://leetcode.com/problems/integer-to-roman/discuss/?currentPage=1&orderBy=hot&query=
 class Solution {
 private:
+    string repeat(const string &str, int times) {
+        stringstream stream;
+
+        for (size_t i = 0; i < times; i++) {
+            stream << str;
+        }
+        return stream.str();
+    }
+
+    string fillNumbers(int current, const string &sig1, const string &sig2,
+                       const string &sig3) {
+        string str;
+        if (current < 4) {
+            str = repeat(sig1, current);
+        } else if (current == 4) {
+            str = sig1 + sig2;
+        } else if (current < 9) {
+            str = sig2;
+            str = str + repeat(sig1, current - 5);
+        } else {
+            str = sig1 + sig3;
+        }
+
+        return str;
+    }
+
 public:
     string intToRoman(int num) {
         string result;
+        int cipher = 0;
         while (true) {
-            int cipher = num / 10;
             int current = num % 10;
             num = num / 10;
+            string current_str;
 
-            string cipher_str;
-            if (10 < cipher && cipher < 100) {
-                cipher = current == 4 ? 'L' : 'C';
-            } else if (100 < cipher && cipher < 1000) {
-                cipher = current == 4 ? 'D' : 'M';
+            if (cipher == 0) {
+                current_str = fillNumbers(current, "I", "V", "X");
+            } else if (cipher == 1) {
+                current_str = fillNumbers(current, "X", "L", "C");
+            } else if (cipher == 2) {
+                current_str = fillNumbers(current, "C", "D", "M");
+            } else if (cipher == 3) {
+                current_str = fillNumbers(current, "M", "I", "I");
             }
 
-            string current_str = "t";
-            if (current < 4) {
-                current_str.append("I", cipher);
-            } else if (current == 4) {
-                if (cipher_str == "L") {
-                    current_str.append("X");
-                } else if (cipher_str == "D") {
-                    current_str.append("C");
-                } else {
-                    current_str.append("I");
-                }
-            } else if (current < 9) {
-                current_str.append("V");
-                current_str.append("I", 9 - cipher);
-            } else if (current == 9) {
-                if (cipher_str == "C") {
-                    current_str.append("X");
-                } else if (cipher_str == "M") {
-                    current_str.append("C");
-                } else {
-                    current_str.append("I");
-                }
-            }
-            cout << current_str << '\n';
-
-            /*
-            if (cipher_str != "") {
-                current_str.append(cipher_str);
-            }
             result = current_str + result;
-             */
-
             if (num == 0) {
                 break;
             }
+
+            cipher++;
         }
         return result;
     }
@@ -65,19 +68,31 @@ public:
 int main() {
     Solution *s;
 
+    /*
     s = new Solution();
     cout << s->intToRoman(3) << '\n';
     delete s;
 
-    /*
     s = new Solution();
     cout << s->intToRoman(4) << '\n';
     delete s;
 
     s = new Solution();
-    cout << s->intToRoman(9) << '\n';
+    cout << s->intToRoman(8) << '\n';
     delete s;
     */
+
+    s = new Solution();
+    cout << s->intToRoman(58) << '\n';
+    delete s;
+
+    s = new Solution();
+    cout << s->intToRoman(1994) << '\n';
+    delete s;
+
+    s = new Solution();
+    cout << s->intToRoman(20) << '\n';
+    delete s;
 
     return 0;
 }
