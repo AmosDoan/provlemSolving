bbaa33bc2a39bfe9a2c9bff8166b8c80d4fd71a0
amos_doan <amos.doan@gmail.com>
2020-02-29 09:13:01 +0900

    - Solving prob11052

--- acmicpc/dp/prob11052.cpp
+++ acmicpc/dp/prob11052.cpp
@@ -12,7 +12,29 @@ vector<vector<int>> dp;
 vector<int> p;
 
 int solve() {
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = 0; j < N + 1; j++) {
+            dp[i][j] = dp[i - 1][j];
 
+            if (i <= j) {
+                int number_of_current = j / i;
+                int when_current_using = number_of_current * p[i - 1] + dp[i - 1][j - number_of_current * i];
+
+                dp[i][j] = max(dp[i][j], when_current_using);
+            }
+        }
+    }
+
+    /*
+    for (int i = 0; i < N + 1; i++) {
+        for (int j = 0; j < N + 1; j++) {
+            cout << dp[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    */
+
+    return dp[N][N];
 }
 
 int main() {
@@ -24,7 +46,7 @@ int main() {
         p.push_back(cost);
     }
 
-    dp.resize(N, vector<int>(N, 0));
+    dp.resize(N + 1, vector<int>(N + 1, 0));
 
     cout << solve() << '\n';
 
