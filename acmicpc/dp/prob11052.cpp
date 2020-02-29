fd54ada39cb4eae0d2b80d3f3e1fb65f5bcbb5fb
amos_doan <amos.doan@gmail.com>
2020-02-29 09:46:16 +0900

    - Add comment for another better solution for prob11052

--- acmicpc/dp/prob11052.cpp
+++ acmicpc/dp/prob11052.cpp
@@ -11,6 +11,22 @@ int N;
 vector<vector<int>> dp;
 vector<int> p;
 
+
+/**
+ * 1차원 풀이법 (중복을 허용하여 선택해 최대값을 만드는 문제인 경우는 뒤에서 부터 접근해 보도록 하자)
+ * https://m.blog.naver.com/PostView.nhn?blogId=occidere&logNo=220786663135&proxyReferer=https%3A%2F%2Fwww.google.com%2F
+ */
+/*
+int solve() {
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            d[i] = max(d[i], d,[i - j] + a[j]);
+        }
+    }
+}
+*/
+
 int solve() {
     for (int i = 1; i < N + 1; i++) {
         for (int j = 0; j < N + 1; j++) {
