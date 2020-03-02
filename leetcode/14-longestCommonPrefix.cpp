faf4e9ce80eaa99399d2f298cdc28b384ef69b6e
amos_doan <amos.doan@gmail.com>
2020-03-02 23:24:20 +0900

    - Solved leetcode 14-longestCommonPrefix

--- leetcode/14-longestCommonPrefix.cpp
+++ leetcode/14-longestCommonPrefix.cpp
@@ -9,7 +9,31 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) {
+            return "";
+        }
 
+        bool is_common = true;
+        string commonPrefix;
+
+        for (int i = 0; i <= strs[0].size(); i++) {
+            char current = strs[0][i];
+
+            for (string str : strs) {
+                if (str[i] != current) {
+                    is_common = false;
+                    break;
+                }
+            }
+
+            if (!is_common) {
+                break;
+            } else {
+                commonPrefix += current;
+            }
+        }
+
+        return commonPrefix;
     }
 };
 
@@ -19,8 +43,14 @@ int main() {
     vector<string> sample = {"flower", "flow", "flight"};
 
     s = new Solution();
-    cout << s->longestCommonPrefix(sample);
-    delete s;
+    cout << "sol : " << s->longestCommonPrefix(sample) << '\n';
+
+    sample = {"dog","racecar","car"};
+    cout << "sol : " << s->longestCommonPrefix(sample) << '\n';
+
+
+    sample = {};
+    cout << "sol : " << s->longestCommonPrefix(sample) << '\n';
 
     return 0;
 }
