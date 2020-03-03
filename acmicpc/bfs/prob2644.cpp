4f3d97c0e0278fdf9116c84b0d594cde68c1fb13
amos_doan <amos.doan@gmail.com>
2020-03-03 21:19:29 +0900

    - Solved prob2644

--- acmicpc/bfs/prob2644.cpp
+++ acmicpc/bfs/prob2644.cpp
@@ -3,16 +3,47 @@
 //
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int N, start, target, M;
 vector<vector<int>> map;
+vector<int> visit;
+queue<pair<int, int>> q;
+
+int solve() {
+
+    q.push({start, 0});
+    visit[start] = 1;
+
+    while (!q.empty()) {
+        pair<int, int> elem = q.front();
+        q.pop();
+
+        int current = elem.first;
+        int level = elem.second;
+
+        if (current == target) {
+            return level;
+        }
+
+        for (int next : map[current]) {
+            if (visit[next] == 0) {
+                visit[next] = 1;
+                q.push({next, level + 1});
+            }
+        }
+    }
+
+    return -1;
+}
 
 int main() {
     cin >> N >> start >> target >> M;
 
     map.resize(N + 1, vector<int>(0, 0));
+    visit.resize(N + 1, 0);
 
     for (int i = 0; i < M; i++) {
         int s, t;
@@ -22,6 +53,7 @@ int main() {
         map[t].push_back(s);
     }
 
+    /*
     for (int i = 0; i< N; i++) {
         vector<int> sub = map[i];
 
@@ -31,6 +63,9 @@ int main() {
         }
         cout << '\n';
     }
+    */
+
+    cout << solve() << '\n';
 
     return 0;
 }
