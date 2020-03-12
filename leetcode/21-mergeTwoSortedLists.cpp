8f2e849b1db65d13d9fba9a5b14f8c9286693903
amos_doan <amos.doan@gmail.com>
2020-03-12 22:16:20 +0900

    - Ready to solve leetcode 21-mergeTwoSortedLists

---
+++ leetcode/21-mergeTwoSortedLists.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Amos on 2020/03/12.
+//
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+    }
+};
+
+int main() {
+    return 0;
+}
