6d4d9b16f7812dc203c4a4769212f2bde573b659
amos_doan <amos.doan@gmail.com>
2020-03-18 22:05:23 +0900

    - Ready to solve leetcode 22-generateParentheses

---
+++ leetcode/22-generateParentheses.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Amos on 2020/03/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
