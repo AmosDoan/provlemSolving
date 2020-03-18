3b92d0daaade2fc11d8a97eacc9a55323399dbd2
amos_doan <amos.doan@gmail.com>
2020-03-18 22:52:57 +0900

    - Solved leetcode 22-generateParentheses

--- leetcode/22-generateParentheses.cpp
+++ leetcode/22-generateParentheses.cpp
@@ -3,17 +3,71 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
+private:
+    void solve(vector<string> &s, string candidate, int left, int valid) {
+        if (candidate.size() == end) {
+            if (valid == 0) {
+                cout << candidate << '\n';
+                s.push_back(candidate);
+            }
+            return;
+        }
+
+        if (valid == 0) {
+            candidate.push_back('(');
+            solve(s, candidate, left - 1, valid + 1);
+            candidate.pop_back();
+        } else if (valid > 0){
+
+            if (left != 0) {
+                candidate.push_back('(');
+                solve(s, candidate, left - 1, valid + 1);
+                candidate.pop_back();
+            }
+
+            candidate.push_back(')');
+            solve(s, candidate, left, valid - 1);
+            candidate.pop_back();
+        } else {
+            cout << "invalid" << '\n';
+        }
+    }
+
+    int end;
 public:
     vector<string> generateParenthesis(int n) {
+        end = 2 * n;
+        vector<string> ret;
 
+        solve(ret, "", n, 0);
+
+        return ret;
     }
 };
 
 int main() {
+    Solution *s;
+
+    s = new Solution();
+    s->generateParenthesis(3);
+    delete s;
+
+    cout << '\n';
+
+    s = new Solution();
+    s->generateParenthesis(2);
+    delete s;
+
+    cout << '\n';
+
+    s = new Solution();
+    s->generateParenthesis(4);
+    delete s;
 
     return 0;
 }
