36872c6ab6da7c2216dd5ce7038c7ec3295c3ff2
amos_doan <amos.doan@gmail.com>
2020-03-24 00:08:01 +0900

    - Ready to solve prob5014

---
+++ acmicpc/bfs/prob5014.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Amos on 2020/03/24.
+//
+#include <iostream>
+
+using namespace std;
+
+int F, S, G, U, D;
+
+int main() {
+    cin >> F >> S >> G >> U >> D;
+
+
+    return 0;
+}
