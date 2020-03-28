2d25e0fde265c01f5deb2b68d17080847d58476d
amos_doan <amos.doan@gmail.com>
2020-03-29 00:12:38 +0900

    - Ready to solve leetcode 24-swapNodesInPairs

---
+++ leetcode/24-swapNodesInPairs.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Amos on 2020/03/29.
+//
+#include <iostream>
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+int main() {
+
+    return 0;
+}
+
