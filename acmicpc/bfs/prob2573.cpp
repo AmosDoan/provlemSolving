3bffb3e9c6fa3c6f812e3c6d732bc887f78b6ba1
amos_doan <amos.doan@gmail.com>
2020-03-28 12:43:46 +0900

    - Solving prob2573

--- acmicpc/bfs/prob2573.cpp
+++ acmicpc/bfs/prob2573.cpp
@@ -16,8 +16,7 @@ queue<pair<int, int>> q;
 int d_row[4] = {-1, 1, 0, 0};
 int d_col[4] = {0, 0, -1, 1};
 
-int bfs() {
-    vector<vector<int>> visit = vector<vector<int>>(N, vector<int>(M, 0));
+void melt(vector<vector<int>> &visit) {
 
     visit[start.first][start.second] = 1;
 
@@ -42,8 +41,8 @@ int bfs() {
 
             int next_val = map[next_row][next_col];
 
-            if (next_val == 0 && current_val > 0) {
-                current_val--;
+            if (next_val == 0) {
+                current_val = current_val > 0 ?  current_val - 1 : current_val;
             } else {
                 visit[next_row][next_col] = 1;
                 q.push({next_row, next_col});
@@ -52,13 +51,83 @@ int bfs() {
     }
 }
 
-int find() {
+void bfs2(int start_row, int start_col, vector<vector<int>> &visit) {
 
+    visit[start_row][start_col] = 1;
+    q.push({start_row, start_col});
+
+    while (!q.empty()) {
+        int current_row = q.front().first;
+        int current_col = q.front().second;
+
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int next_row = current_row + d_row[i];
+            int next_col = current_col + d_col[i];
+            int next_val = map[next_row][next_col];
+
+            if (next_row < 0 || next_row > N - 1 || next_col < 0 || next_col > M - 1) {
+                continue;
+            }
+
+            if (visit[next_row][next_col] != 0) {
+                continue;
+            }
+
+            if (next_val == 0) {
+                continue;
+            }
+
+            visit[next_row][next_col] = 1;
+            q.push({next_row, next_col});
+        }
+    }
+}
+
+int find() {
+    int level = 0;
+    vector<vector<int>> visit = vector<vector<int>>(N, vector<int>(M, 0));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] != 0 && visit[i][j] == 0) {
+                level++;
+                if (level == 2) {
+                    return level;
+                }
+                bfs2(i, j, visit);
+            }
+        }
+    }
+    return level;
+}
+
+void findStart() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] != 0) {
+                start = {i, j};
+                return;
+            }
+        }
+    }
 }
 
 int solve() {
-    q.push({start.first, start.second});
-    bfs();
+    int time = 0;
+    while (true) {
+        findStart();
+        vector<vector<int>> visit = vector<vector<int>>(N, vector<int>(M, 0));
+        q.push({start.first, start.second});
+
+        time++;
+        melt(visit);
+
+        int num_ice = find();
+        if (num_ice > 1 || num_ice == 0) {
+            return time;
+        }
+    }
 }
 
 int main() {
@@ -68,23 +137,12 @@ int main() {
         for (int j = 0; j < M; j++) {
             int elem;
             cin >> elem;
-            if (elem != 0 && start == make_pair(0, 0)) {
-                start = {i, j};
-            }
-
             row.push_back(elem);
         }
         map.push_back(row);
     }
 
-    solve();
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cout << map[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+    cout << solve() << '\n';
 
     return 0;
 }
