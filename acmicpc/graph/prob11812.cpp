5033782e8918106e7f5a0644d7d938d252191501
amos_doan <amos.doan@gmail.com>
2020-03-28 23:53:09 +0900

    - Solved prob11812

--- acmicpc/graph/prob11812.cpp
+++ acmicpc/graph/prob11812.cpp
@@ -2,53 +2,41 @@
 // Created by Amos on 2020/03/28.
 //
 #include <iostream>
-#include <set>
 
 using namespace std;
 
 long long N, K, Q;
 
 long long solve(long long node1, long long node2) {
-    set<long long> s;
-    long long ancestor = node1;
-
-    while (true) {
-        s.insert(ancestor);
-        ancestor = (ancestor - 2) / K + 1;
-
-        if (ancestor == 1) {
-            s.insert(1);
-            break;
+    long long result = 0;
+    while (node1 != node2) {
+        if (node1 > node2) {
+            node1 = (node1 - 2) / K + 1;
+        } else {
+            node2 = (node2 - 2) / K + 1;
         }
+
+        result++;
     }
 
-    long long common_ancestor = 1;
-    ancestor = node2;
-    while (ancestor != 1) {
-        s.insert(ancestor);
-        ancestor = (ancestor - 2) / K + 1;
-        if (s.find(ancestor) != s.end() || ancestor == 1) {
-            common_ancestor = ancestor;
-            break;
-        }
-    }
-
-    set<long long> result;
-    for (auto i = s.begin(); i != s.end(); ++i) {
-        if (*i >= common_ancestor) {
-            result.insert(*i);
-        }
-    }
-
-    return result.size() - 1;
+    return result;
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N >> K >> Q;
     for (int i = 0; i < Q; i++) {
         long long node1 = 0, node2 = 0;
         cin >> node1 >> node2;
-        cout << solve(node1, node2) << '\n';
+
+        if (1LL == K) {
+            cout << abs(node1 - node2) << '\n';
+        } else {
+            cout << solve(node1, node2) << '\n';
+        }
     }
 
     return 0;
