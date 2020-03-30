95ef7c38653cff7f8c3276ef2911cad1ffb1b79b
amos_doan <amos.doan@gmail.com>
2020-03-31 00:10:25 +0900

    - Solved prob1517

--- acmicpc/sorting/prob1517.cpp
+++ acmicpc/sorting/prob1517.cpp
@@ -1,48 +1,84 @@
 //
 // Created by Amos on 2019/12/27.
 //
-#include <cstdio>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-/*
- * N의 범위가 50000 (5 X 10^4)까지 이므로
- * 아래와 같이 실제 bubble sorting을 실행하면 O(N^2) => 25 X 10^8 이므로 1초가 넘어버린다.
- * 머지 소트를 이용해야 함
- */
-int bubbleSort(vector<int> &v) {
-    int res = 0;
+int N;
+vector<int> buf;
+vector<int> temp;
+long long nr_swap = 0;
 
-    for (int i = v.size() - 1; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            if (v[j] > v[j + 1]) {
-                int temp = v[j];
-                v[j] = v[j + 1];
-                v[j + 1] = temp;
-                res++;
+void mergeSort(int first, int second, int size) {
+    int first_current = first;
+    int second_current = second;
+    int insert = first;
+    int end = first + size;
+
+    while (first_current < second && second_current < end) {
+        if (buf[first_current] < buf[second_current]) {
+            temp[insert] = buf[first_current];
+            first_current++;
+        } else {
+            if (buf[first_current] != buf[second_current]) {
+                nr_swap += second_current - insert;
             }
+            temp[insert] = buf[second_current];
+            second_current++;
+        }
+        insert++;
+    }
+
+    if (first_current >= second) {
+        for (int i = second_current; i < end; i++) {
+            temp[insert] = buf[i];
+            insert++;
+        }
+    } else {
+        for (int i = first_current; i < second; i++) {
+            temp[insert] = buf[i];
+            insert++;
         }
     }
 
-    return res;
+    for (int i = first; i < end; i++) {
+        buf[i] = temp[i];
+    }
+}
+
+void solve(int start, int end) {
+    if (start < end) {
+        int middle = (end + start) / 2;
+        solve(start, middle);
+        solve(middle + 1, end);
+        mergeSort(start, middle + 1, end - start + 1);
+    }
+
 }
 
 int main() {
-    int N;
-    vector<int> buf;
-
-    scanf("%d\n", &N);
+    cin >> N;
 
     buf.resize(N);
+    temp.resize(N);
 
     for (int i = 0; i < N; i++) {
         int a;
-        scanf("%d", &a);
+        cin >> a;
         buf[i] = a;
     }
 
-    printf("%d\n", bubbleSort(buf));
+    solve(0, N - 1);
 
+    /*
+    for (int i = 0; i < N; i++) {
+        cout << buf[i] << ' ';
+    }
+    cout << '\n';
+     */
+
+    cout << nr_swap << '\n';
     return 0;
 }
