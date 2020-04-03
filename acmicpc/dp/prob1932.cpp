98a930021c8974b999717154245c7eb62f76b219
amos_doan <amos.doan@gmail.com>
2020-04-03 23:11:00 +0900

    - Solved prob1932

--- acmicpc/dp/prob1932.cpp
+++ acmicpc/dp/prob1932.cpp
@@ -8,14 +8,35 @@ using namespace std;
 
 int N;
 vector<vector<int>> numbers;
+vector<vector<int>> cache;
 
-int solve() {
-    return 0;
+int solve(int y, int x) {
+    if (y == N) {
+        return numbers[y][x];
+    }
+
+    int &ret = cache[y][x];
+    if (ret != -1) {
+        return ret;
+    }
+
+    return ret = max(solve(y + 1, x), solve(y + 1, x + 1)) + numbers[y][x];
 }
 
 int main() {
     cin >> N;
+    numbers.resize(N + 1, vector<int>(N + 1, 0));
+    cache.resize(N + 1, vector<int>(N + 1, -1));
 
-    cout << solve() << '\n';
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j < i + 1; j++) {
+            int c;
+            cin >> c;
+            numbers[i][j] = c;
+        }
+    }
+
+    cout << solve(0, 0) << '\n';
+
     return 0;
 }
