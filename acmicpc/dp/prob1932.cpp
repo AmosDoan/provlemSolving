d6f0936b37216988e0e5a67e0527738afc7d2eff
amos_doan <amos.doan@gmail.com>
2020-04-03 22:38:09 +0900

    - Ready to solve prob1932

---
+++ acmicpc/dp/prob1932.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Amos on 2020/04/03.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<vector<int>> numbers;
+
+int solve() {
+    return 0;
+}
+
+int main() {
+    cin >> N;
+
+    cout << solve() << '\n';
+    return 0;
+}
