802badd62afd57bfc05ad26c21698f3ce7b5cd00
amos_doan <amos.doan@gmail.com>
2020-04-05 20:31:46 +0900

    - Solved leetcode 122 with optimal solution

--- leetcode/122-bestTimeToBuyAndSellStock2.cpp
+++ leetcode/122-bestTimeToBuyAndSellStock2.cpp
@@ -45,7 +45,6 @@ public:
 
     vector<int> cache;
 };
-*/
 
 class Solution {
 public:
@@ -86,6 +85,25 @@ public:
     }
 
     vector<int> cache;
+};
+*/
+
+// Greedy. Optimal Solution
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        for (int i = 0; i < prices.size() - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                maxProfit += prices[i + 1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
 };
 
 int main() {
