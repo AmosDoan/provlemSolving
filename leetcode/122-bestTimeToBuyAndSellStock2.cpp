cc1e8071b21761e5a1367a38ea3b03009eb4203a
amos_doan <amos.doan@gmail.com>
2020-04-05 20:12:57 +0900

    - Solved leetcode 122-bestTimeToBuyAndSellStock2

--- leetcode/122-bestTimeToBuyAndSellStock2.cpp
+++ leetcode/122-bestTimeToBuyAndSellStock2.cpp
@@ -7,30 +7,6 @@
 using namespace std;
 
 class Solution {
-private:
-    int solve(int current, vector<int>& prices) {
-        if (current <= 0) {
-            return 0;
-        }
-
-        int &ret = cache[current];
-        if (ret != -1) {
-            return ret;
-        }
-
-        for (int i = 0; i < current; i ++) {
-            int latest = prices[current] - prices[i];
-            latest += solve(i - 1, prices);
-
-            if (latest > ret) {
-                ret = latest;
-            }
-        }
-
-        ret = max(solve(current - 1, prices), ret);
-        return max(ret, 0);
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() <= 1) {
@@ -39,7 +15,33 @@ public:
 
         cache.resize(prices.size(), -1);
         cache[0] = 0;
-        return solve(prices.size() - 1, prices);
+
+        int ret = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            int current = prices[i];
+            int temp_max = 0;
+
+            if (current == 0) {
+                cache[i] = cache[i - 1];
+                ret = max(temp_max, ret);
+                continue;
+            }
+
+            for (int j = 0; j < i; j++) {
+                int temp = current - prices[j];
+                if (j > 0) {
+                    temp += cache[j - 1];
+                }
+
+                temp = max(temp , 0);
+                temp_max = max(temp, temp_max);
+                temp_max = max(temp_max, cache[i - 1]);
+            }
+            cache[i] = temp_max;
+            ret = max(temp_max, ret);
+        }
+
+        return ret;
     }
 
     vector<int> cache;
@@ -65,6 +67,12 @@ int main() {
     p = {7, 6, 4, 3, 1};
     cout << s->maxProfit(p) << '\n';
     delete s;
+
+    s = new Solution();
+    p = {3, 2, 6, 5, 0, 3};
+    cout << s->maxProfit(p) << '\n';
+    delete s;
+
 
     return 0;
 }
