51d94d6ef56aab88a76c20b685b11114d7428335
amos_doan <amos.doan@gmail.com>
2020-04-06 18:36:33 +0900

    - Solved leetcode 49

--- leetcode/49-groupAnagrams.cpp
+++ leetcode/49-groupAnagrams.cpp
@@ -3,13 +3,53 @@
 //
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+// map은 red black tree로 매번 정렬 O(nlogn) unordered map은 hash table로 O(1)
 
 using namespace std;
 
 class Solution {
+private:
+    string countSort(string &str) {
+        string ret;
+
+        int charCount[26] = { 0 };
+
+        for (int i = 0; i < str.length(); i++) {
+            charCount[str[i] - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < charCount[i]; j++) {
+                ret += (char)('a' + i);
+            }
+        }
+        return ret;
+    }
+
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
+        unordered_map<string, vector<string>> m;
+
+        for (auto str : strs) {
+            string sorted = countSort(str);
+
+            if (m.find(sorted) == m.end()) {
+                vector<string> v;
+                v.push_back(str);
+                m[sorted] = v;
+            } else {
+                m[sorted].push_back(str);
+            }
+        }
+
+        vector<vector<string>> ret;
+        for (auto & iter : m) {
+            ret.push_back(iter.second);
+        }
+
+        return ret;
     }
 };
 
@@ -21,6 +61,11 @@ void printResult(vector<vector<string>> &&ret) {
         cout << '\n';
     }
 }
+
+static const auto _ = []() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
 
 int main() {
     Solution *s;
