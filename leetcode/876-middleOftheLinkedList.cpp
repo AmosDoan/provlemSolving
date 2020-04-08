6d721d5db7f952aa6bc149d71e220cc10ab3625b
amos_doan <amos.doan@gmail.com>
2020-04-08 17:58:32 +0900

    - Ready to solve 876-middleOftheLinkedList

---
+++ leetcode/876-middleOftheLinkedList.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Amos on 2020/04/08.
+//
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+    }
+};
+
+int main() {
+    Solution *s;
+
+
+    return 0;
+}
