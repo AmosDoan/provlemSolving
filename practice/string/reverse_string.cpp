2f3ec0ffb630f6967a425c5de7a6767874b12326
amos_doan <amos.doan@gmail.com>
2020-04-12 11:56:09 +0900

    - Add reverse_string

---
+++ practice/string/reverse_string.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Amos on 2020/04/12.
+//
+#include <iostream>
+
+using namespace std;
+
+void reverseString(char *str) {
+    char *start = str;
+    char *end = str + strlen(str) - 1;
+
+    while (start < end) {
+        char buf;
+
+        buf = *start;
+        *start = *end;
+        *end = buf;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+
+    // String literal (read only)
+    //char *s = "abc";
+
+    char s[] = "abc";
+    reverseString(s);
+    cout << s << '\n';
+
+    return 0;
+}
