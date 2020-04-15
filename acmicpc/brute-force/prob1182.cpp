422a411178d00bab395e960ba2dc323f30802cb5
amos_doan <amos.doan@gmail.com>
2020-04-15 12:39:43 +0900

    - Solving prob1182

--- acmicpc/brute-force/prob1182.cpp
+++ acmicpc/brute-force/prob1182.cpp
@@ -1,4 +1,56 @@
 //
 // Created by Amos on 2020/04/15.
 //
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int N, S;
+vector<int> numbers;
+
+int solve() {
+    vector<int> sum;
+    sum.push_back(numbers[0]);
+
+   for (int i = 1; i < numbers.size(); i++) {
+       int temp_sum = numbers[i] + sum[i - 1];
+       sum.push_back(temp_sum);
+   }
+
+   /*
+   for (auto s : sum) {
+       cout << s << ' ';
+   }
+   cout << '\n';
+   */
+
+   int ret = 0;
+   for (int i = 0; i < sum.size(); i++) {
+       if (sum[i] == S) {
+           ret++;
+       }
+
+       for (int j = i + 1; j < sum.size(); j++) {
+           int partial_sum = sum[j] - sum[i];
+           if (partial_sum == S) {
+               ret++;
+           }
+       }
+   }
+   return ret;
+}
+
+int main() {
+    cin >> N >> S;
+
+    for (int i = 0; i < N; i++) {
+        int c = 0;
+        cin >> c;
+        numbers.push_back(c);
+    }
+
+    cout << solve() << '\n';
+
+    return 0;
+}
