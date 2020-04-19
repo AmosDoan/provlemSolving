b1b4aa82618814b3dee2d93b863654b4854f456a
amos_doan <amos.doan@gmail.com>
2020-04-19 17:59:26 +0900

    - Solved leetcode 33

--- leetcode/33-searchInRotatedSortedArray.cpp
+++ leetcode/33-searchInRotatedSortedArray.cpp
@@ -8,33 +8,29 @@ using namespace std;
 
 class Solution {
 private:
+    int solve(vector<int>& nums, int start, int end, int target) {
+        int middle = (start + end) / 2;
+        if (nums[start] > nums[end]) {
+            int left = solve(nums, start, middle, target);
+            if (left != -1) {
+                return left;
+            }
+            return solve(nums, middle + 1, end, target);
+        } else {
+            return binarySearch(nums, start, end, target);
+        }
+    }
+
     int binarySearch(vector<int>& nums, int start, int end, int target) {
         if (end < start) {
             return -1;
         }
-
         int mid = (start + end) / 2;
         if (nums[mid] == target) {
             return mid;
-        } else if (nums[mid] < target && target <= nums[end]) {
+        } else if (nums[mid] < target) {
             return binarySearch(nums, mid + 1, end, target);
-        } else if (nums[mid] > target && target >= nums[start]) {
-            return binarySearch(nums, start, mid - 1, target);
-        }
-
-        // left compare
-        if (nums[mid] > target && nums[start] > target) {
-            if (nums[end] < target) {
-                return -1;
-            }
-
-            return binarySearch(nums, mid + 1, end, target);
-        }
-
-        if (nums[mid] < target && nums[end] < target) {
-            if (nums[start] > target) {
-                return -1;
-            }
+        } else if (nums[mid] > target) {
             return binarySearch(nums, start, mid - 1, target);
         }
 
@@ -43,7 +39,15 @@ private:
 
 public:
     int search(vector<int>& nums, int target) {
-        return binarySearch(nums, 0, nums.size() - 1, target);
+        if (nums.empty()) {
+            return -1;
+        }
+
+        if (nums.size() == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        return solve(nums, 0, nums.size() - 1, target);
     }
 };
 
@@ -51,7 +55,6 @@ int main() {
     Solution *s;
     vector<int> v;
 
-    /*
     s = new Solution();
     v = {4, 5, 6, 7, 0, 1, 2};
     cout << s->search(v, 0) << '\n';
@@ -71,7 +74,6 @@ int main() {
     v = {5, 1, 3};
     cout << s->search(v, 5) << '\n';
     delete s;
-    */
 
     s = new Solution();
     v = {4, 5, 6, 7, 8, 1, 2, 3};
@@ -81,3 +83,5 @@ int main() {
 
     return 0;
 }
+
+static int x = [] () {ios::sync_with_stdio(false); cin.tie(0); return 0;} ();
