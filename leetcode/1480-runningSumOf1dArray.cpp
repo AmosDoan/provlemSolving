b802bfef781fc1ff53ff2641547e7c1015e088b9
amos_doan <amos.doan@gmail.com>
2020-08-17 22:27:26 +0900

    - Solved 1480-runningSumOf1dArray

---
+++ leetcode/1480-runningSumOf1dArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+};
+
+int main() {
+    Solution *s;
+
+    s = new Solution();
+
+
+    delete s;
+}
+
+static const auto _ = []() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
